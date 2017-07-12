8f6b2f288c4f29c0b51daedff0589feeb4648861
taomin <taomin@360.cn>
2017-07-12 18:53:25 +0800

    maxSumSerialArray 最大连续子串和 code commit

--- src/pub/dynamic_plan/max_sub_array/max_sub_array.go
+++ src/pub/dynamic_plan/max_sub_array/max_sub_array.go
@@ -2,8 +2,10 @@ package pub
 
 import (
 	"errors"
+	"fmt"
 )
 
+// 最大子串和(非连续)
 func maxSumArray(a []int) (int, error) {
 	if len(a) == 0 {
 		return 4294967295, errors.New("lenght of array is zero")
@@ -23,5 +25,32 @@ func maxSumArray(a []int) (int, error) {
 			maxSum = currSum
 		}
 	}
+	return maxSum, nil
+}
+
+// 最大连续子串和
+func maxSumSerialArray(a []int) (int, error) {
+	var start, end int
+	if len(a) == 0 {
+		return 4294967295, errors.New("lenght of array is zero")
+	}
+	if len(a) == 1 {
+		return a[0], nil
+	}
+	var maxSum int = a[0]
+	var currSum int = 0
+	for k, v := range a {
+		if currSum+v <= v {
+			currSum = v
+			start = k
+		} else {
+			currSum += v
+		}
+		if maxSum < currSum {
+			maxSum = currSum
+			end = k
+		}
+	}
+	fmt.Print(a[start : end+1])
 	return maxSum, nil
 }
--- src/pub/dynamic_plan/max_sub_array/max_sub_array_test.go
+++ src/pub/dynamic_plan/max_sub_array/max_sub_array_test.go
@@ -30,6 +30,22 @@ func Test_maxSumArray_3(t *testing.T) {
 	}
 }
 
+func Test_maxSumSerialArray_1(t *testing.T) {
+	var a []int = []int{1, -2, 3, 10, -4, 7, 2, -5}
+	b, err := maxSumSerialArray(a)
+	if err == nil {
+		fmt.Println(b)
+	}
+}
+
+func Test_maxSumSerialArray_2(t *testing.T) {
+	var a []int = []int{1, -10, 1, 1, 1}
+	b, err := maxSumSerialArray(a)
+	if err == nil {
+		fmt.Println(b)
+	}
+}
+
 func Test_insertSort_4(t *testing.T) {
 	var a []int = []int{0}
 	b, err := maxSumArray(a)
